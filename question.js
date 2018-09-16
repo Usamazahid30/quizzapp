1a39a38887a990df1686de35f37bae58a4713376
usamazahid30 <zahidusama20@gmail.com>
2018-09-16 13:46:16 +0500

    first

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "Programmer who works with high level languages and have better understanding with applications are considered as ?",
+	"option1": "Design programmer",
+	"option2": "Application programmer",
+	"option3": "Analysis programmer",
+	"option4": "Train programmer",
+	"answer": "2"
+}, {
+	"question": "Programmer who works with high level languages and have better understanding with applications are considered as ?",
+	"option1": "Terminal control staff",
+	"option2": "Data control staff",
+	"option3": "Peripheral control staff",
+	"option4": "Librarian control staff",
+	"answer": "2"
+}, {
+	"question": "Which is a single integrated circuit?",
+	"option1": "Gate",
+	"option2": "Mother Board",
+	"option3": "Chip",
+	"option4": "CPU",
+	"answer": "1"
+}, {
+	"question": "C is ?",
+	"option1": "A third generation high level language",
+	"option2": "A machine language",
+	"option3": "An assembly language",
+	"option4": "All of the above",
+	"answer": "1"
+}, {
+	"question": "Web pages are written using ?",
+	"option1": "FTP",
+	"option2": "HTTP",
+	"option3": "HTML",
+	"option4": "URL",
+	"answer": "3"
+}, {
+	"question": "UNIVAC is an example of",
+	"option1": "First generation computer",
+	"option2": "Second generation computer",
+	"option3": "Third generation computer",
+	"option4": "Fourth generation computer",
+	"answer": "1"
+}, {
+	"question": "Which of the following is an example of non volatile memory ?",
+	"option1": "VLSI",
+	"option2": "LSI",
+	"option3": "ROM",
+	"option4": "RAM",
+	"answer": "3"
+}, {
+	"question": "Graphic interfaces were first used in a xerox product is called ?",
+	"option1": "Ethernet",
+	"option2": "Inter LISP",
+	"option3": "Small talk",
+	"option4": "Zeta LISP",
+	"answer": "1"
+}, {
+	"question": "Find the odd one out ?",
+	"option1": "ORACLE",
+	"option2": "SYBASE",
+	"option3": "C",
+	"option4": "INFORMIX",
+	"answer": "3"
+}, {
+	"question": "Person who keeps disc and tape files up to date for computer installation is called",
+	"option1": "File librarian",
+	"option2": "Punch librarian",
+	"option3": "Peripheral librarian",
+	"option4": "Termianl librarian",
+	"answer": "1"
+}];
